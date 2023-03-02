8ef668ec6ba6ad1bba1fd0b6a65a7aaf67e66612
serendipity <2045027920@qq.com>
2023-03-02 15:46:14 +0800

    23.3.2

--- HomeWorrk/Lecture1/pingpong.c
+++ HomeWorrk/Lecture1/pingpong.c
@@ -3,15 +3,38 @@
 
 int main(int argc, char* argv[]){
 	int p1[2],int p2[2];
-	pipe[p1];
-	pipe[p2];
-	char buf[1];  //restore signal;
-	if(fork()!=0){ //enter parent process
-		close(p1[1]）;   //关闭p1写端
-		read(p1[0], 0 ,1);  //从标准输入里面写入信号
+	pipe(p1);
+	pipe(p2);
+	char buf[1]=" ";  
+	if(fork()>0){ //父进程中
+		close(p1[0]）;   //关闭p1读端
+		close(p2[0]);	//关闭p2写端
+		close(p2[1]);	//关闭p2读端	
+		write(p1[1],buf,1)	//将buf的信号写入pipe中
+		
+		read(p2[0],buf,1);	//将信号从管道中读入到buf中
+		printf("%d : received pong",gitpid());
+		
+		close(p1[1]);
+		close(p2[0]);
+		
+		exit(0);
+		
 	}
 	
-	else if(fork()>0){
+	else if(fork() == 0){//子进程中
+		close(p1[1]);	//关闭p1写端
+		close(p2[0]);	//关闭p2读端
+		read(p1[0], buf,1];	//从管道中读信号到buf中
+		printf("%d : received ping",getpid());	//接收信号输出
+		
+		write(p2[1],buf,1);	//将信号从buf中写入到管道中
+		
+		close(p1[0]);
+		close(p2[1]);
+		exit(0);
+		
+		
 		
 	}
 	
