de84a7d967482d66fc7fdfd4dfb70b89430bb13c
serendipity <2045027920@qq.com>
2023-03-03 16:09:57 +0800

    23.3.3-2

--- HomeWorrk/Lecture1/primes.c
+++ HomeWorrk/Lecture1/primes.c
@@ -1,34 +1,60 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]){
+#define PRIME_NUM 35
+
+int main(int argc, char * argv[]){
 	int p[2];
+	char buf;
 	pipe(p);
-	char buf[2];
 	
-	if (fork()>0){
-		close(p[0]); //关闭父进程管道读端
-		for(int i=2;i<36;i++){
-			if(i%2==0)
-				continue;
-			write(p[1],"0"+i,1);  //将buf中的数读入管道
-			printf("primes : %d", 2);
-		}
-		wait(); //等待子程序执行完
-		exit(0);	//退出
+	if(fork()==0){//子进程中
+		close(p[1]);	//关闭写端
+		child(&p[0]);
+		exit(0);
 	}
-	else if(fork()==0){
-		close(p[1]);	//关闭子进程管道写端
-		if(read(p[0],&buf[0],1)){
-			
-		};	//将管道中的数据读入buf[0]中
-		while(read(p[1],&buf[1],1){
-			
+	else{//最初的父进程中
+		close(p[0]);  //关闭读端
+		for(int i=2;i<PRIME_NUM+1;i++){
+			write(p[1],"0"+i,1); //将初始数据写入管道中
 		}
 		
+		close(p1[1]);
+		wait(int*(0));
 		
 	}
+	exit(0);
+}
+
+void child(int *p){
+	int pr[2];
+	char prime;
+	if(read(p,&prime,1))
+			printf("primes: %c\n",prime);
 	else{
-		exit(1);
+		exit(0); //没有数据可以读出，退出
+	}
+	
+	
+	if(fork()==0){//子进程中
+		close(p);
+		close(pr[1]);
+		child(*pr[0]);
+		close(pr[0]);
+		exit(0);
+	}
+	
+	else{//父进程中
+		close(pr[0]);
+		char buf;
+		while(!read(p,&buf,1)){
+			if(atoi(buf)%atoi(prime)==0){
+				write(pr[1],buf,1);
+			}
+		}
+		close(p);
+		close(pr[1]);
+		wait(int*(0));
+		exit(0);
 	}
 }
